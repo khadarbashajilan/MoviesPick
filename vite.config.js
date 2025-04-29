88ff940215400b25cd46f35a0fe757346e99d083
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-04-29 19:21:07 +0530

    deployment buf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/movies-pick/',
+  base: '/',
   server: {
     // For dev server
     historyApiFallback: true,
